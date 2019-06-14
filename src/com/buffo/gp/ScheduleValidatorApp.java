8e3f4f8923347f6380baaf1be577386fe361c4eb
GP Buffo <gp@labs.epiuse.com>
2019-06-14 09:45:39 +0200

    Refactor implementation and re-create executable JAR

--- src/com/buffo/gp/ScheduleValidatorApp.java
+++ src/com/buffo/gp/ScheduleValidatorApp.java
@@ -7,45 +7,57 @@ import java.util.Scanner;
 
 public class ScheduleValidatorApp {
 
+    private static final String INTERVAL_START_PROMPT =
+            "Enter a start date/time for your interval in the format HH:mm:ss (e.g. 13:30:00)";
+    private static final String INTERVAL_END_PROMPT =
+            "Enter an end date/time for your interval in the format HH:mm:ss (e.g. 17:45:00)";
+    private static final String TIME_TO_VALIDATE_PROMPT =
+            "Enter the date/time you'd like to validate in the format HH:mm:ss (e.g. 16:30:00)";
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss");
+
     public static void main(String[] args) {
         System.out.println("***** Welcome to the Schedule Validator *****");
         System.out.println("Close the application at any time by pressing Ctrl + C\n");
 
         Scanner scanner = new Scanner(System.in);
 
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-        LocalTime intervalStart = null;
-        LocalTime intervalEnd = null;
-        LocalTime dateTimeToValidate = null;
+        LocalTime intervalStart = getLocalTime(scanner, INTERVAL_START_PROMPT);
+        LocalTime intervalEnd = getLocalTime(scanner, INTERVAL_END_PROMPT);
+        LocalTime timeToValidate = getLocalTime(scanner, TIME_TO_VALIDATE_PROMPT);
 
-        while (intervalStart == null) {
-            System.out.println("Enter a start date/time for your interval in the format HH:mm:ss (e.g. 13:30:00)");
+        boolean isTimeWithinInterval = isTimeWithinInterval(intervalStart, intervalEnd, timeToValidate);
+
+        System.out.println(getOutputMessage(isTimeWithinInterval));
+    }
+
+    private static LocalTime getLocalTime(
+            Scanner scanner,
+            String prompt
+    ) {
+        LocalTime time = null;
+
+        while (time == null) {
+            System.out.println(prompt);
             try {
-                intervalStart = LocalTime.parse(scanner.next(), dateTimeFormatter);
+                time = LocalTime.parse(scanner.next(), DATE_TIME_FORMATTER);
             } catch (DateTimeParseException exception) {
                 System.out.println("Incorrect format specified");
             }
         }
 
-        while (intervalEnd == null) {
-            System.out.println("Enter an end date/time for your interval in the format HH:mm:ss (e.g. 17:45:00)");
-            try {
-                intervalEnd = LocalTime.parse(scanner.next(), dateTimeFormatter);
-            } catch (DateTimeParseException exception) {
-                System.out.println("Incorrect format specified");
-            }
-        }
+        return time;
+    }
 
-        while (dateTimeToValidate == null) {
-            System.out.println("Enter the date/time you'd like to validate in the format HH:mm:ss (e.g. 17:45:00)");
-            try {
-                dateTimeToValidate = LocalTime.parse(scanner.next(), dateTimeFormatter);
-            } catch (DateTimeParseException exception) {
-                System.out.println("Incorrect format specified");
-            }
-        }
+    private static boolean isTimeWithinInterval(
+            LocalTime intervalStart,
+            LocalTime intervalEnd,
+            LocalTime timeToValidate
+    ) {
+        return timeToValidate.compareTo(intervalStart) >= 0 && timeToValidate.compareTo(intervalEnd) <= 0;
+    }
 
-        boolean isDateTimeWithinInterval = dateTimeToValidate.compareTo(intervalStart) >= 0 && dateTimeToValidate.compareTo(intervalEnd) <= 0;
-        System.out.println(isDateTimeWithinInterval ? "true" : "false");
+    private static String getOutputMessage(boolean isTimeWithinInterval) {
+        return isTimeWithinInterval ? "Time is within the specified interval" : "Time is not within the specified interval";
     }
 }
